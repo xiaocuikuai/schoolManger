a8dbb401dac42c97676fb34899d62e40b332e493
czd <czd17429989@163.com>
2021-05-12 21:51:04 +0800

    1

---
+++ src/main/java/com/wdd/studentmanager/util/AjaxResult.java
@@ -0,0 +1,41 @@
+package com.wdd.studentmanager.util;
+
+
+public class AjaxResult {
+    private boolean success;
+    private String message;
+    private String imgurl;
+    private String type;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/wdd/studentmanager/util/DateFormatUtil.java
@@ -0,0 +1,12 @@
+package com.wdd.studentmanager.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class DateFormatUtil {
+    public static String getFormatDate(Date date, String format){
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+}
